878c52e5f1c4a9ba73b605927da50d1691210c49
Vyacheslav <Programmer38rus@gmail.com>
2020-09-15 21:52:41 +0800

    c

--- log.py
+++ log.py
@@ -1,8 +1,17 @@
 import logging
+from bottle import route, run, request
+import sentry_sdk
+from sentry_sdk.integrations.bottle import BottleIntegration
+
+sentry_sdk.init(
+    "https://079e09b3c0684ca8a241681891dc7913@o448256.ingest.sentry.io/5429334",
+    integrations=[BottleIntegration()],
+    traces_sample_rate=1.0
+)
 
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s --- %(message)s")
 
-logger = logging.getLogger("my_logger")
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 class MyFilter:
@@ -11,7 +20,6 @@ class MyFilter:
 
 
 steam_handler = logging.StreamHandler()
-# steam_handler = logging.FileHandler("aksjklsjf.gg")
 steam_handler.setFormatter(formatter)
 steam_handler.setLevel(logging.DEBUG)
 
@@ -24,9 +32,21 @@ logger.addHandler(file_handler)
 logger.addHandler(steam_handler)
 
 
+# logger.debug("Проверка того, что сообщения уровня DEBUG обрабатываются и логером и обработчиком")  
+# logger.info("Тестовое сообщение уровня INFO")  
+# logger.error("Ещё одно сообщение, но уже уровня ERROR")
+# logger.critical("вот еще одно")
 
+@route('/success')
+def index():
+    # logger.debug(request.headers.get("User-Agent"))
+    # return (request.headers.get("User-Agent"))
+    return
 
-logger.debug("Проверка того, что сообщения уровня DEBUG обрабатываются и логером и обработчиком")  
-logger.info("Тестовое сообщение уровня INFO")  
-logger.error("Ещё одно сообщение, но уже уровня ERROR")
-logger.critical("вот еще одно")
+@route('/fail')
+def fail():
+
+    raise RuntimeError("This is bad")
+
+    return
+run(host='localhost', port=9999)
---
+++ sentry_test.py
@@ -0,0 +1,7 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    "https://079e09b3c0684ca8a241681891dc7913@o448256.ingest.sentry.io/5429334",
+    traces_sample_rate=1.0
+)
+print(a)
